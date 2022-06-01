65754b53634942d3e8c1f95d3a10cdf454aeec9a
mattbcoding <mattbodden@googlemail.com>
2022-06-01 23:06:08 +0100

    Add initial django packages and settings config

---
+++ druid/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for druid project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'druid.settings')
+
+application = get_asgi_application()
