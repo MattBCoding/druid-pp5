ee155aa52ae8e871afc3da590930f892d7685810
mattbcoding <mattbodden@googlemail.com>
2022-06-18 18:28:08 +0100

    Add product management page and linked in nav

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
+from operator import is_
 from django.shortcuts import render
 from .models import Product, Category
+from django.contrib.admin.views.decorators import staff_member_required
 
 # Create your views here.
 
@@ -8,8 +10,22 @@ def all_products(request):
     products home page
     contains all available products
     '''
-    products = Product.objects.all()
+    # only return active products
+    products = Product.objects.all().filter(is_active=True)
     context = {
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+@staff_member_required
+def product_management(request):
+    '''
+    View to access product management page
+    and options for product management
+    '''
+    product_list = Product.objects.all()
+    context = {
+        'product_list': product_list,
+    }
+    return render(request, 'products/product_management.html', context)
+
