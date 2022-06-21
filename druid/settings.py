a9542e4983d20674a16f0a2dbae663290d8d4d75
mattbcoding <mattbodden@googlemail.com>
2022-06-21 22:56:36 +0100

    Add initial Stripe configuration

--- druid/settings.py
+++ druid/settings.py
@@ -268,6 +268,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Stripe Checkout and Bag Variables
 FREE_DELIVERY_THRESHOLD = 0
 STANDARD_DELIVERY_PERCENTAGE = 5
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
