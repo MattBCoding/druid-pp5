a6252c8ad6f4904cc19a6a4af0e6ecfc2db71434
mattbcoding <mattbodden@googlemail.com>
2022-06-28 14:15:36 +0100

    Style base, nav and footer

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+// function to handle moving the nav content around depending on screen size
 function moveNavContent() {
     let topContainer = document.getElementById('top-nav-container');
     let mobileContainer = document.getElementById('mobile-top-nav-container');
@@ -8,58 +9,51 @@ function moveNavContent() {
     if (viewportWidth < breakpoint) {
         if (mobileContainer.innerHTML != content) {
             mobileContainer.appendChild(content);
-            console.log('moved from if statement into mobile container');
         }
     }
     if (viewportWidth >= breakpoint) {
         if (topContainer.innerHTML != content) {
             topContainer.appendChild(content);
-            console.log('moved from if statement into top container');
         }
     }
     window.addEventListener('resize', () => {
         let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
         if (viewportWidth < breakpoint) {
             mobileContainer.appendChild(content);
-            console.log('moved from eventlistener into mobileContainer');
         } else if (viewportWidth >= breakpoint) {
             topContainer.appendChild(content);
-            console.log('moved from eventlistener into topContainer');
         }
     })
 }
 
+// function to display the search box on request
 function displaySearch() {
-    console.log('search icon clicked');
     let searchForm = document.getElementById('search-form');
     let searchFormContainer = document.getElementById('search-form-container');
     searchFormContainer.style.display = 'block';
     searchForm.style.display = 'flex';
     searchForm.addEventListener('click', (e) => {
-        console.log('searchForm clicked');
         e.stopImmediatePropagation();
     })
     searchFormContainer.addEventListener('click', () => {
-        console.log('searchFormContainer clicked');
         searchForm.style.display = 'none';
         searchFormContainer.style.display = 'none';
     })
 }
 
+// function to close modal inserted by htmx
 function closeModal() {
 	var container = document.getElementById("modals-here")
-	// var backdrop = document.getElementById("modal-backdrop")
 	var modal = document.getElementById("modal")
 
 	modal.classList.remove("show")
-	// backdrop.classList.remove("show")
 
 	setTimeout(function() {
-		// container.removeChild(backdrop)
 		container.removeChild(modal)
 	}, 200)
 }
 
+// function to prevent click through modal inserted by htmx
 function stopPropagation(event) {
     event.stopImmediatePropagation()
 }
