afb28f913a7b14510c572f7faf17b326829c54d1
mattbcoding <mattbodden@googlemail.com>
2022-06-28 01:35:45 +0100

    Add AWS configuration

--- druid/settings.py
+++ druid/settings.py
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'django_summernote',
     'django_filters',
+    'storages',
 
 ]
 
@@ -269,6 +270,30 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+if 'USE_AWS' in os.environ:
+    # Cache Control 
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    # Bucket Configuration
+    AWS_STORAGE_BUCKET_NAME = 'ci-project-five-druid-computers'
+    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_LOCATION = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLS in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 
 
 # Static files (CSS, JavaScript, Images)
