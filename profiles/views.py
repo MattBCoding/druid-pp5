7630b8f11820eafb4708c6c24b93c49b224cd98a
mattbcoding <mattbodden@googlemail.com>
2022-06-30 11:16:32 +0100

    Fix line length validator errors

--- profiles/views.py
+++ profiles/views.py
@@ -10,7 +10,8 @@ from .utils import getAddresses
 from products.models import Product
 
 User = get_user_model()
-# Create your views here.
+
+
 @login_required
 def profile(request):
     profile = get_object_or_404(User, pk=request.user.id)
@@ -24,13 +25,20 @@ def profile(request):
     if request.method == 'POST':
         address_id = request.POST.get('address')
         if request.user.address.filter(default__exact=True).exists():
-            currentDefaultAddress = get_object_or_404(Address, user=request.user, default=True)
+            currentDefaultAddress = get_object_or_404(
+                                                      Address,
+                                                      user=request.user,
+                                                      default=True)
             currentDefaultAddress.default = False
             currentDefaultAddress.save(update_fields=['default'])
-        updateDefaultAddress = get_object_or_404(Address, user=request.user, id=address_id)
+        updateDefaultAddress = get_object_or_404(
+                                                 Address,
+                                                 user=request.user,
+                                                 id=address_id)
         updateDefaultAddress.default = True
         updateDefaultAddress.save(update_fields=['default'])
     return render(request, 'profiles/profile.html', context)
+
 
 @login_required
 def addAddress(request):
@@ -48,10 +56,13 @@ def addAddress(request):
             # check to see if a default address exists before save
             if request.user.address.filter(default__exact=True).exists():
                 # change previous default address default field to false
-                if address.default == True:
-                    currentDefaultAddress = get_object_or_404(Address, user=request.user, default=True)
-                    currentDefaultAddress.default = False
-                    currentDefaultAddress.save(update_fields=['default'])
+                if address.default is True:
+                    defaultAddress = get_object_or_404(
+                                                       Address,
+                                                       user=request.user,
+                                                       default=True)
+                    defaultAddress.default = False
+                    defaultAddress.save(update_fields=['default'])
                     # save new address once old one has been updated
                     address.save()
                 else:
@@ -59,10 +70,12 @@ def addAddress(request):
             else:
                 address.default = True
                 address.save()
-            # insert confirmation message to user confirming address saved successfully
+            messages.success(request, 'Your address has been successfully \
+                                updated')
             return redirect('profile')
-    
+
     return render(request, 'profiles/address_form.html', context)
+
 
 @login_required
 def editAddress(request, pk):
@@ -71,18 +84,15 @@ def editAddress(request, pk):
         if request.method == 'POST':
             form = AddressForm(request.POST, instance=address)
             if form.is_valid():
-                print('form is valid')
                 if form.data.get('default', False):
-                    print('data equals true')
-                    currentDefaultAddress = get_object_or_404(Address, user=request.user, default=True)
-                    print('got previous default address')
-                    if address != currentDefaultAddress:
-                        print('there is another address that is default')
-                        currentDefaultAddress.default = False
-                        currentDefaultAddress.save(update_fields=['default'])
-                        print('other address removed as default')
+                    defaultAddress = get_object_or_404(
+                                                       Address,
+                                                       user=request.user,
+                                                       default=True)
+                    if address != defaultAddress:
+                        defaultAddress.default = False
+                        defaultAddress.save(update_fields=['default'])
                 form.save()
-                print('form saved')
                 return redirect('profile')
         else:
             form = AddressForm(instance=address)
@@ -94,12 +104,14 @@ def editAddress(request, pk):
     else:
         return redirect('home')
 
+
 @login_required
 def deleteAddress(request, pk):
     address = get_object_or_404(Address, pk=pk)
     if address.user == request.user:
         address.delete()
         return redirect('profile')
+
 
 @login_required
 def deleteUserAccount(request, pk):
@@ -119,6 +131,7 @@ def deleteUserAccount(request, pk):
             else:
                 messages.error(request, 'Please enter your email address')
 
+
 @login_required
 def editUserAccount(request, pk):
     user = get_object_or_404(User, pk=pk)
@@ -131,11 +144,13 @@ def editUserAccount(request, pk):
                 form.save()
                 messages.success(request, 'Your details have been updated')
                 return redirect('profile')
-        
+
         return render(request, 'profiles/edit_user_form.html', context)
     else:
-        messages.error(request, 'Error - you can not access that users account')
+        messages.error(request, 'Error - \
+            you can not access that users account')
         return redirect('profile')
+
 
 @login_required
 def favourite(request, pk):
@@ -167,6 +182,7 @@ def favourite(request, pk):
         return render(request, 'profiles/my_favourites.html', context)
 
     return HttpResponseRedirect(reverse('product_detail', args=[slug]))
+
 
 @login_required
 def my_favourites(request):
